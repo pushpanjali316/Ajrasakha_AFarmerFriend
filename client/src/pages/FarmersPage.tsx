ce7780e4aee9feb50aef1234087f078f0b91c870
pushpanjali316 <pushpanjalivandavasi@gmail.com>
2026-06-27 22:31:38 +0530

    Fix css file import in FarmersPage.tsx

--- client/src/pages/FarmersPage.tsx
+++ client/src/pages/FarmersPage.tsx
@@ -1,5 +1,5 @@
 import React,{useEffect,useState} from "react";
-import "../styles/farmersPage.css";
+import "../styles/farmerspage.css";
 import { API_BASE } from "../services/api";
 
 interface Farmer{
